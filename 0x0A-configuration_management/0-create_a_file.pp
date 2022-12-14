029692a88810a3a3edfbbc16e272942fc6e53dc6
dinka09 <dinkawolde09@gmail.com>
2022-12-14 11:25:17 +0000

    added #ensure

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Create a file
 file {'/tmp/school':
-mode => '0744'
-owner => 'www-data'
-group => 'www-data'
-content => 'I love Puppet'
+  ensure => 'file'
+  mode => '0744'
+  owner => 'www-data'
+  group => 'www-data'
+  content => 'I love Puppet'
 }
 
